8e8edc59a62e51257b6ecf5d83d652579ca17700
Kasun M <kasunldrago77@gmail.com>
2022-09-10 10:30:56 +0530

    Payment App genarated

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -10,12 +10,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api/seller';
+  const globalPrefix = 'api/payment';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 4444;
   await app.listen(port);
   Logger.log(
-    `🚀 Seller API is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Payment API is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
